6435ef60b0b995463912b17c81d29076b8a1d719
Carlos Gabriel <carlosggflor@gmail.com>
2018-11-12 22:32:25 -0200

    Added Method Lagrange

--- src/main.js
+++ src/main.js
@@ -2,33 +2,34 @@
  * resolve interpolation with method lagrange
  * @param {*} params 
  */
-function calcLagrange(input,table) {
-    console.log('%c Calc lagrange','color:red;font-size:2em;');
-    throw "Not Implemented"
+function calcLagrange() {
+    let values = this.getValues();
+    let px = this.getPx();
+    let matrix = this.arrayToMatrix(values, 2);
+    let final = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        let actualL = i;
+        let result = 1;
+        for (let x = 0; x < matrix.length; x++) {
+            if (!(x === actualL)) {
+                let resultL = (px - matrix[x][0]) / (matrix[actualL][0] - matrix[x][0]);
+                result *= resultL;
+            }
+        }
+        final += matrix[i][1] * result;
+    }
+
+    alert(`Resultado Final: ${final.toFixed(2)}`)
+
 }
 
 /**
  * resolve interpolation with newton method
  * @param {*} params 
  */
-function calcNewton(input,table) {
-    console.log('%c Calc Newton','color:red;font-size:2em;');
-    throw "Not Implemented"
-}
-
-/**
- * retrieve all values table
- * @param {*} params 
- */
-function retrieveTable() {
-    throw "Not Implemented"
-}
-
-/**
- * retrieve input value of {x}
- * @param {*} params 
- */
-function retrieveInput() {
+function calcNewton(input, table) {
+    console.log('%c Calc Newton', 'color:red;font-size:2em;');
     throw "Not Implemented"
 }
 
@@ -37,7 +38,53 @@ function retrieveInput() {
  * @param {*} params 
  */
 function clearTable() {
+    let table = this.getInputs();
+    table.forEach(value => value.value = '');
+}
+
+function getPx() {
+    return Number(document.querySelector('#px').value);
+}
+
+function getInputs() {
     let htmlCollectionTd = document.querySelectorAll('td input');
     let arrayHtmlCollection = [...htmlCollectionTd];
-    arrayHtmlCollection.forEach(value => value.value = '');
+    return arrayHtmlCollection;
+}
+
+function getValues() {
+    let table = this.getInputs();
+    let values = table.filter((input) => {
+        return input.value !== "";
+    }).map(input => {
+        return Number(input.value);
+    })
+
+    if (!this.isPair(values)) {
+        alert(" Numero impar de entradas. ");
+        return;
+    }
+
+    return values;
+}
+
+function arrayToMatrix(arr, size) {
+    return arr.reduce((rows, key, index) => (index % size == 0 ? rows.push([key])
+        : rows[rows.length - 1].push(key)) && rows, []);
+}
+
+/**
+ * populate table with predefinied values
+ * @param {*} params 
+ */
+function populate() {
+
+    let input = [1, -1, 1.1, 1.51, 1.3, 2.56, 1.4, -3.1];
+    let array = document.querySelectorAll('table td input');
+    array.forEach((v, i, a) => { if (i < 10) v.value = input[i] });
+
+}
+
+function isPair(array) {
+    return array.length % 2 === 0;
 }
